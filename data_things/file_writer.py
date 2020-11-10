85739ee2f5b7b0423daca6c9e1ca040fd0a98f62
trholdridge <trholdridge@gmail.com>
2020-11-09 21:31:26 -0500

    new laptop test

--- data_things/file_writer.py
+++ data_things/file_writer.py
@@ -8,9 +8,11 @@ Created on Tue Oct 20 21:50:39 2020
 
 from data_collection import *
 
-# write data file    
+# write data file
 with open("phonology_data.csv", "w") as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(['Language', 'Consonants'])
     for lang, consonants in phonologies.items():
         writer.writerow([lang, consonants])
+
+# test change
